721cc0219a160065c8f1b95cbf996e12cc378781
Bevinda Juliani Enge <BE40202X@cimbniaga.co.id>
2020-06-22 17:26:05 +0700

    verifikasi user

--- src/main/java/com/cimb/tokolapak/entity/User.java
+++ src/main/java/com/cimb/tokolapak/entity/User.java
@@ -4,70 +4,79 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-//@Table(name="users")
+// @Table(name="users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	private String username;
-	
-//	@JsonIgnore
+
+	// @JsonIgnore
 	private String password;
-	
+
 	private String email;
 
 	private String profilePicture;
-	
+
 	private boolean isVerified;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
 	}
-	
+
 	public String getProfilePicture() {
 		return profilePicture;
 	}
+
 	public void setProfilePicture(String profilePicture) {
 		this.profilePicture = profilePicture;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public boolean isVerified() {
 		return isVerified;
 	}
+
 	public void setVerified(boolean isVerified) {
 		this.isVerified = isVerified;
 	}
-	
+
 }
